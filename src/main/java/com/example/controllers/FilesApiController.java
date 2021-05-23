3030dc7762b905d10d591d757ab15bc2edf61a0b
rajjaiswalsaumya <rajjaiswalsaumya@gmail.com>
2021-05-23 17:42:37 +0530

    Add FilesApiController

---
+++ src/main/java/com/example/controllers/FilesApiController.java
@@ -0,0 +1,26 @@
+package com.example.controllers;
+
+import com.example.services.ResourceLocatorService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.io.IOException;
+import java.nio.file.Path;
+import java.util.stream.Stream;
+
+@RestController()
+@RequestMapping("/files")
+public class FilesApiController {
+
+    private final ResourceLocatorService resourceLocatorService;
+
+    public FilesApiController(ResourceLocatorService resourceLocatorService) {
+        this.resourceLocatorService = resourceLocatorService;
+    }
+
+    @GetMapping("/list")
+    public Stream<Path> list() throws IOException {
+        return this.resourceLocatorService.list();
+    }
+}
